1267f7cde456da04dbe0cca4717bd5f7e81b3b1e
Francisco Iacobelli <francisco@iacobelli.cl>
2016-02-16 09:40:25 -0600

    specifies end of lines

--- micro_sats.py
+++ micro_sats.py
@@ -1,7 +1,7 @@
 import sys
 import itertools
 from pprint import pprint
-import re
+import re,math
 
 
 def dna_combo(n):
@@ -40,18 +40,20 @@ def find_all_ms(seq, dna, num_reps,lflank=40):
         s = e
     return ms_dict
 
-def find_all_ms_re(seq,dna,num_reps,lflank=40):
+def find_all_ms_re(seq,dna,num_reps,lflank=40,target_cg=0.5,m_length=16,max_length=24):
     pat = re.compile(r'('+seq+'){'+str(num_reps)+',}')
     ms_dict={}
     for m in pat.finditer(dna):
         s,e,gr= (m.start(),m.end(),m.group())
-        reps = (s-e)/len(seq)
+        reps = (e-s)/len(seq)
         ms_dict[seq] = ms_dict.get(seq, [])
         sa=s-40 if s>lflank else 0
         ep=e+40 if e<len(dna)-lflank else len(dna)
-        cga=cg_content(dna[sa:s])
-        cgp=cg_content(dna[e:ep])
-        ms_dict[seq].append((s, e,reps,cga,cgp))
+        cgfo=cg_content(dna[sa:s])
+        cgre=cg_content(dna[e:ep])
+        opt_fcg,opt_f=find_best_primer(dna[sa:s],m_length,max_length,target_cg)
+        opt_rcg,opt_r=find_best_primer(dna[e:ep],m_length,max_length,target_cg)
+        ms_dict[seq].append((s, e,reps,cgre,cgfo,opt_f,opt_fcg,opt_r,opt_rcg))
     return ms_dict
 
 
@@ -73,15 +75,46 @@ def cg_content(region_string):
 
 def file_save(l_dict,ofile):
     f = open(ofile, 'w')
-    f.write("microsat\tstart\tend\treps\tcg_ant\tcg_post")
+    f.write("microsat\tstart\tend\treps\tcg_forward\tcg_reverse\topt_forward_seq\tcg_content\topt_reverse_seq\tcg_content")
     for key,val in l_dict.items():
-        for start,end,reps,cga,cgp in val:
-            f.write("\n%s\t%d\t%d\t%d\t%f\t%f"%(key,start,end,reps,cga,cgp))
+        for s, e,reps,cgre,cgfo,opt_f,opt_fcg,opt_r,opt_rcg in val:
+            f.write("\n%s\t%d\t%d\t%d\t%f\t%f\t%s\t%f\t%s\t%f"%(key,s, e,reps,cgre,cgfo,opt_f,opt_fcg,opt_r,opt_rcg))
     f.close()
 
 
+def ngrams(txt, n):
+    output = []
+    for i in range(len(txt)-n+1):
+        output.append(txt[i:i+n])
+    return output
+
+
+def dna_windows(seq,min_length,max_length):
+    " Returns all the sequences of length l (min_length < l <max_length) in seq"
+    seqs = []
+    for i in range(min_length,max_length+1):
+        seqs.extend(ngrams(seq,i))
+    return seqs
+
+def optimum_cgc(seqs,target_cg):
+    "Returns the sequence in seqs that has the closest cg content to target_cg"
+    seql = [((cg_content(s)-target_cg)**2,s) for s in seqs]
+    cgc,seq=min(seql)
+    return (math.sqrt(cgc)+target_cg,seq)
+
+
+def find_best_primer(seq,min_length,max_length,target_cg):
+    return optimum_cgc(dna_windows(seq,min_length,max_length),target_cg)
+
+
+def open_deNovo(fname):
+    with open(fname) as f:
+        lines=[l.strip() for l in f if ">" not in l]
+    return "~".join(lines)
+
+
 def main(dna, n, reps,ofile):
-    d = find_combinations(2, dna, reps)
+    d = find_combinations(n, dna, reps)
     #pprint(d)
     file_save(d,ofile)
 
@@ -90,7 +123,13 @@ if __name__== "__main__":
     n=int(n)
     reps = int(reps)
     if len(sys.argv) > 3:
-        dna = open(dnafile).read()
-        main(dna, n, reps,ofile)
-
+        dna = open_deNovo(dnafile)
+        print "Looking for repetions of "+str(n)+" base pairs"
+        if n!=0:
+           main(dna, n, reps,ofile)
+        else:
+           for i in [2,3,4]:
+               main(dna,i,reps,ofile+".%d"%i)
+    print "Done"
+                
 
